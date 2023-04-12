130cda24886bf1073c753968d6f8003e7c48c014
greatmomo <moritzkalt37@gmail.com>
2023-04-12 21:30:23 +0200

    Initial commit

---
+++ Player/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+class_name Player
+
+var speed = 80
+var jump_impulse = 360
+var gravity = 1200
+var acceleration = 60
+var friction = 20
+var air_friction = 10
+
+func get_input_direction() -> float:
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	
+	if direction < 0:
+		$Sprite2D.flip_h = true
+	if direction > 0:
+		$Sprite2D.flip_h = false
+	
+	return direction
